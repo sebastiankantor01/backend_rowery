5a3f4dbdc4b13989b618f10d15354311f018742e
achwi00 <achwierut00@gmail.com>
2024-11-05 14:27:06 +0100

    changes_ac: [feat] added methods in UserDAO

--- src/main/java/projekt/servlet/UserServlet.java
+++ src/main/java/projekt/servlet/UserServlet.java
@@ -1,10 +1,13 @@
 package projekt.servlet;
 
+import projekt.dao.UserDAO;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 @WebServlet("/api/users/*")
 public class UserServlet extends HttpServlet
 {
+    private UserDAO userDAO;
 
 }
