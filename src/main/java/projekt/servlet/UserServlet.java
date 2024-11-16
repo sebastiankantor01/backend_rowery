627d4774b1ba2b47c951b8acb77efc8a5706096d
awhite03 <149816436+awhite03@users.noreply.github.com>
2024-11-16 17:18:44 +0100

    users-by-id-endpoint

--- src/main/java/projekt/servlet/UserServlet.java
+++ src/main/java/projekt/servlet/UserServlet.java
@@ -47,4 +47,39 @@ public class UserServlet extends HttpServlet
             resp.getWriter().write("Błąd serwera: " + e.getMessage());
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String pathInfo = req.getPathInfo();
+
+        if (pathInfo == null || pathInfo.split("/").length != 2) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("Wymagany jest identyfikator użytkownika w ścieżce URL.");
+            return;
+        }
+
+        try {
+            int userId = Integer.parseInt(pathInfo.split("/")[1]);
+
+            // Pobierz użytkownika po ID z DAO
+            Users user = userDAO.getUserById(userId);
+
+            if (user != null) {
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                resp.getWriter().write(objectMapper.writeValueAsString(user));
+                resp.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                resp.getWriter().write("User o id " + userId + " nie został znaleziony.");
+            }
+        } catch (SQLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write("Błąd serwera: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("Identyfikator użytkownika musi być liczbą.");
+        }
+    }
 }
